7b70b2cb0760514beb6feef1ab096b6c6d935d8f
Dave Herman <dherman@mozilla.com>
2017-01-17 11:43:11 -0800

    use https

--- foreign/foreign-module.js
+++ foreign/foreign-module.js
@@ -1,7 +1,7 @@
 (function() {
 
 var script = document.createElement('script');
-script.src = 'http://xoscript.surge.sh/secret-module.js';
+script.src = 'https://xoscript.surge.sh/secret-module.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 })();
