6c950ad87aa06e853db788cf982b5dc1140654a0
Dave Herman <dherman@mozilla.com>
2017-01-17 11:51:29 -0800

    also demonstrate leaked XO function source

--- foreign/foreign-module.js
+++ foreign/foreign-module.js
@@ -5,3 +5,9 @@ script.src = 'https://xoscript.surge.sh/secret-module.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 })();
+
+window.foreignModuleFunction = function() {
+    var tmp = "string in foreign module";
+    console.log("foreign module function");
+};
+
--- foreign/secret-module.js
+++ foreign/secret-module.js
@@ -5,3 +5,9 @@ p.textContent = 'The secret module has been loaded.';
 document.body.appendChild(p);
 
 })();
+
+window.secretModuleFunction = function() {
+    var tmp = "string in secret module";
+    console.log("secret module function");
+};
+
