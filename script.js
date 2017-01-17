1611e2a23f281334dd3f1a08236e6eb4edda031d
Dave Herman <dherman@mozilla.com>
2017-01-17 15:22:42 -0800

    eliminate logging stuff?

--- script.js
+++ script.js
@@ -58,16 +58,3 @@ class Loader {
 }
 
 loader = new Loader();
-
-// attempt to read the source of remote content
-function inspectForeignSource() {
-  var p = document.createElement('p');
-  p.innerText = "Let's print the source code to some remotely loaded functions:";
-  document.body.appendChild(p);
-
-  [foreignModuleFunction, secretModuleFunction].forEach((f) => {
-    var pre = document.createElement('pre');
-    pre.innerText = f.toString();
-    document.body.appendChild(pre);
-  });
-}
