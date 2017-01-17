d1966949c960e090fd850b349f4eb9581d0621bc
Dave Herman <dherman@mozilla.com>
2017-01-17 12:36:29 -0800

    debugging

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
+function setupChannel() {
+  return new Promise((resolve, reject) => {
+    var chan = new MessageChannel();
+
+    chan.port1.onmessage = event => {
+      if (event.data.error) {
+        reject(event.data.error);
+      } else {
+        resolve(event.data);
+      }
+    };
+
+    console.log("sending port to service worker");
+    navigator.serviceWorker.controller.postMessage("hello", [chan.port2]);
+  });
+}
+
 (function() {
 
-  function setupChannel() {
-    return new Promise((resolve, reject) => {
-      var chan = new MessageChannel();
 
-      chan.port1.onmessage = event => {
-        if (event.data.error) {
-          reject(event.data.error);
-        } else {
-          resolve(event.data);
-        }
-      };
-
-      console.log("sending port to service worker");
-      navigator.serviceWorker.controller.postMessage("hello", [chan.port2]);
-    });
-  }
-
-  setupChannel()
-    .then(data => {
-      var p = document.createElement('p');
-      p.innerHTML = "<b>Service Worker:</b> " + data;
-      document.body.appendChild(p);
-    })
-    .catch(console.log.bind(console));
 
 })();
 
